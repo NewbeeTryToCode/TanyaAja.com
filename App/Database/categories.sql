977979b7c48856be797d994a6f7edd442481bee0
laksmanayudha <laksmanayudha22@gmail.com>
2020-12-14 21:51:27 +0800

    perbaharui tabel

--- App/Database/categories.sql
+++ App/Database/categories.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2020 at 03:32 PM
+-- Generation Time: Dec 14, 2020 at 02:48 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -38,16 +38,30 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `question_id`) VALUES
-(1, 'php', 8),
-(2, 'java', 8),
-(10, 'dede', 10),
 (13, 'json', 14),
 (14, 'array', 14),
 (33, 'python', 16),
 (34, 'java', 16),
-(35, 'jangan dijawab', 9),
-(36, 'random', 9),
-(37, 'gajelas', 9);
+(38, 'json', 17),
+(39, 'javascript', 17),
+(40, 'python', 18),
+(41, 'python', 19),
+(42, 'array', 19),
+(43, 'python', 20),
+(44, 'matrix', 20),
+(45, 'Haskell', 22),
+(46, 'C++', 22),
+(50, 'html', 37),
+(51, 'button', 37),
+(52, 'remove', 37),
+(53, 'input', 38),
+(54, 'button', 38),
+(55, 'html', 38),
+(56, 'border', 38),
+(57, 'remove', 38),
+(73, 'jangan dijawab', 9),
+(74, 'random', 9),
+(75, 'gajelas', 9);
 
 --
 -- Indexes for dumped tables
@@ -67,7 +81,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
