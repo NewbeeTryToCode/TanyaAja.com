cefeb8aa50d62d34cab9b2d182f40635aecbcc49
NewbeeTryToCode <36224565+NewbeeTryToCode@users.noreply.github.com>
2020-12-17 18:48:18 +0800

    mengedit login.php dan menambahkan signup.php

--- App/CRUD/Auth/login.inc.php
+++ App/CRUD/Auth/login.inc.php
@@ -1,61 +1,67 @@
 <?php
-    include "../konek.db.php";
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-        function validate($data){
-            $data = trim($data);
-            $data = stripslashes($data);
-            $data = htmlspecialchars($data);
-            return $data;
-        }
-
-        $user = validate($_POST['username']);
-        $pwd = validate($_POST['password']);
-
-        if (empty($user) && empty($pwd)) {
-            header("Location: ../../../Public/index.php?error=username & password required");
-            exit();
-        }else if(empty($user)){
-            header("Location: ../../../Public/index.php?error=user name is required");
-            exit();
-        }elseif (empty($pwd)) {
-            header("Location: ../../../Public/index.php?error=password is required");
-            exit();
-        }
-        else{
-            $sql = "SELECT * FROM users WHERE username='$user' AND password='$pwd'";
-            $result = mysqli_query($conn, $sql);
-
-            if (mysqli_num_rows($result)) {
-                $row = mysqli_fetch_assoc($result);
-                if ($row['username'] === $user && $row['password'] === $pwd) {
-                    if ($row['role'] === 'admin') {
-                        session_start();
-                        $_SESSION['username'] = $row['username'];
-                        $_SESSION['role'] = $row['role'];
-                        $_SESSION['id'] = $row['id'];
-                        $_SESSION['login'] = true;
-                        header("Location: ../../Views/admin_manage_question.php");
-                        exit(); 
-                    }elseif($row['role'] === 'user'){
-                        session_start();
-                        $_SESSION['username'] = $row['username'];
-                        $_SESSION['role'] = $row['role'];
-                        $_SESSION['id'] = $row['id'];
-                        $_SESSION['login'] = true;
-                        header("Location: ../../Views/public_questions.php");
-                        exit();
-                    }else{
-                        header("Location: ../../../Public/index.php?error=wrong usertype");
-                        exit();
-                    }
-                }
-            }else{
-                header("Location: ../../../Public/index.php?error=incorrect username and password");
-                exit();
-            }
-        }
-    }else{
-        header("Location: ../../../Public/index.php?error=no user");
-        exit();
+include "../konek.db.php";
+session_start();
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
-?>
+
+    $user = validate($_POST['username']);
+    $pwd = validate($_POST['password']);
+
+    if (empty($user) && empty($pwd)) {
+        $_SESSION['errormsg'] = "username & password required";
+        header("Location: ../../Views/login.php");
+        exit();
+    } else if (empty($user)) {
+        $_SESSION['errormsg'] = "username required";
+        header("Location: ../../Views/login.php");
+        exit();
+    } elseif (empty($pwd)) {
+        $_SESSION['errormsg'] = "password required";
+        header("Location: ../../Views/login.php");
+        exit();
+    } else {
+        $sql = "SELECT * FROM users WHERE username='$user' AND password='$pwd'";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result)) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row['username'] === $user && $row['password'] === $pwd) {
+                if ($row['role'] === 'admin') {
+                    session_start();
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['role'] = $row['role'];
+                    $_SESSION['id'] = $row['id'];
+                    $_SESSION['login'] = true;
+                    header("Location: ../../Views/admin_manage_question.php");
+                    exit();
+                } elseif ($row['role'] === 'user') {
+                    session_start();
+                    $_SESSION['username'] = $row['username'];
+                    $_SESSION['role'] = $row['role'];
+                    $_SESSION['id'] = $row['id'];
+                    $_SESSION['login'] = true;
+                    header("Location: ../../Views/public_questions.php");
+                    exit();
+                } else {
+                    $_SESSION['errormsg'] = "Wrong user type";
+                    header("Location: ../../Views/login.php");
+                    exit();
+                }
+            }
+        } else {
+            $_SESSION['errormsg'] = "incorrect username and password";
+            header("Location: ../../Views/login.php");
+            exit();
+        }
+    }
+} else {
+    $_SESSION['errormsg'] = "No user";
+    header("Location: ../../Views/login.php");
+    exit();
+}
---
+++ App/CRUD/Auth/signup.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+include "../konek.db.php";
+
+if (isset($_POST['username']) && isset($_POST['password']) && isset($_POST['email'])) {
+  function validate($data)
+  {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+  $user = validate($_POST['username']);
+  $pwd = validate($_POST['password']);
+  $email = validate($_POST['email']);
+
+
+  if (empty($user) && empty($pwd) && empty($email)) {
+    $_SESSION['errormsg'] = "username, email, & password required";
+    header("Location: ../../Views/signup.php");
+    exit();
+  } else if (empty($user) && empty($pwd)) {
+    $_SESSION['errormsg'] = "username & password required";
+    header("Location: ../../Views/signup.php");
+    exit();
+  } else if (empty($email) && empty($pwd)) {
+    $_SESSION['errormsg'] = "email & password required";
+    header("Location: ../../Views/signup.php");
+    exit();
+  } else if (empty($user) && empty($email)) {
+    $_SESSION['errormsg'] = "username & email required";
+    header("Location: ../../Views/signup.php");
+    exit();
+  } else if (empty($user)) {
+    $_SESSION['errormsg'] = "username required";
+    header("Location: ../../Views/signup.php");
+    exit();
+  } elseif (empty($pwd)) {
+    $_SESSION['errormsg'] = "password required";
+    header("Location: ../../Views/signup.php");
+    exit();
+  } elseif (empty($email)) {
+    $_SESSION['errormsg'] = "email required";
+    header("Location: ../../Views/signup.php");
+    exit();
+  } else {
+    //cek di db apakah user sudah ada
+    $user_check_query = "SELECT * FROM users WHERE username = '$user' OR email='$email' LIMIT 1";
+    $result = mysqli_query($conn, $user_check_query);
+    $userlist = mysqli_fetch_assoc($result);
+    //cek user
+    if ($userlist) {
+      //cek username
+      if ($userlist['username'] === $user) {
+        $_SESSION['errormsg'] = "username already exists";
+        header("Location: ../../Views/signup.php");
+        exit();
+      }
+      //cek email
+      if ($userlist['email'] === $email) {
+        $_SESSION['errormsg'] = "email already exists";
+        header("Location: ../../Views/signup.php");
+        exit();
+      }
+    } // buka tudak ada error
+    else {
+      //encrypt password sebelum masuk db
+      $password = md5($pwd);
+      $query = "INSERT INTO users (username,email,password) VALUES('$user','$email','$password')";
+      mysqli_query($conn, $query);
+      $_SESSION['successmsg'] = "user berhasil dibuat";
+      header("Location: ../../Views/login.php");
+      exit();
+    }
+  }
+}
